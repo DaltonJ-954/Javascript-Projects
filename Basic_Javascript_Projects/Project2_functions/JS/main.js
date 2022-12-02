c62e3b675b399d7f44145840162d0826339b72b1
Dalton Walls <scallywig@gmail.com>
2022-12-01 22:25:53 -0500

    Project2 commit

---
+++ Basic_Javascript_Projects/Project2_functions/JS/main.js
@@ -0,0 +1,15 @@
+function myFunction() {
+    var greet1="Morning ";
+    var greet2="How's it going!";
+    var greet3 = greet1 + greet2;
+
+document.getElementById("demo").innerHTML = myFunction = greet3;
+}
+
+function greet(name) {
+    window.alert("knock KNOCK! " + name);
+}
+
+greet('Mario');
+
+
