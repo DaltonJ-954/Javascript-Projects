a6767841a669e65f08c099894fe7b9ff538ef283
Dalton Walls <scallywig@gmail.com>
2022-12-02 23:34:39 -0500

    commit Project4 code

---
+++ Basic_Javascript_Projects/Project4_dictionaries/JS/main.js
@@ -0,0 +1,11 @@
+function my_List() { //name of function
+    Animal = { //
+        Species:"Reptile", 
+        Color:"Brown",
+        Type:"Komodo",
+        Weight:"90lbs",
+        Length:"10ft"
+    };
+    delete Animal.Type; // This allows you to delete and line of code without going into the JS file
+    document.getElementById("List").innerHTML = Animal.Type;
+}
