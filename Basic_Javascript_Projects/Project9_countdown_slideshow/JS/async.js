856a475a8e7e5cac3e9b06424d5b403f0575c4d6
Dalton Walls <scallywig@gmail.com>
2022-12-06 19:50:36 -0500

    added more more

---
+++ Basic_Javascript_Projects/Project9_countdown_slideshow/JS/async.js
@@ -0,0 +1,11 @@
+function Vaccine() {
+   input = (vaccinated = 3);
+   counter = 0;
+    while(counter < vaccinated) {
+    print("Congratulations!");
+    counter = counter + 1;
+    print("You are vaccinated, YAY!");
+    document.getElementById("Covid").innerHTML = "";
+  }
+}
+   
---
+++ Basic_Javascript_Projects/Project9_countdown_slideshow/JS/new.js
@@ -0,0 +1,5 @@
+function  moon() {
+    var Pluto = 9;
+    var Jupiter = 5;
+    document.getElementById("mars").innerHTML = (Pluto + Jupiter);
+}
