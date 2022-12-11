4af0f5c05feb086e09a65eeeb49f2232d8551254
Dalton Walls <scallywig@gmail.com>
2022-12-10 21:25:02 -0500

    calculator project commit

---
+++ JavaScript/JS/JavaScript_1.js
@@ -0,0 +1,4 @@
+function Good_Morning_Function() {
+    var Z = document.getElementsByClassName("Press");
+    Z[0].innerHTML = "The message was modified!";
+}
---
+++ JavaScript/JS/int_float.js
@@ -0,0 +1,6 @@
+let address = "1735 nw 14th circle";
+let floater = "524.662.344.787";
+let cost = 12.45;
+let Animal = ["007 I have a message", "123", "302", "114.523"];
+
+console.log(parseFloat(Animal));
