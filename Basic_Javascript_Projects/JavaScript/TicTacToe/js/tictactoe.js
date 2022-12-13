406b365123a9de910bdc4bf7d15cbfb5f0a47575
Dalton Walls <scallywig@gmail.com>
2022-12-13 18:45:22 -0500

    TicTac corrections made

--- Basic_Javascript_Projects/JavaScript/TicTacToe/js/tictactoe.js
+++ Basic_Javascript_Projects/JavaScript/TicTacToe/js/tictactoe.js
@@ -23,24 +23,24 @@ function placeXOrO(squareNumber) {
         //squareNumbaer and activePlayer are concatenated together and added to array.
         selectedSquares.push(squareNumber + activePlayer);
         //This calls a function to check for any win conditions.
-        checkWinCondition();
+        checkWinConditions();
         //This condition is for changing the active player.
         if (activePlayer === 'X') {
             //If active player is 'X' change it to 'O'.
             activePlayer = 'O';
             //if active is anything other than 'X'
-        }else {
+        } else {
             //Change the activePlayer to 'X'
             activePlayer = 'X';
         }
        //This function plays placement sound.
-       Audio('./media/move.mp3');
+       audio('./media/move.mp3');
        //This condition checks to see if it is the computers turn.
        if (activePlayer === 'O') {
             //This function disables clicking for computers turn.
             disableClick();
             //This function waits 1 second before the computer places an image and enables click.
-            setTimeout(function () { computerTurn(); }, 1000);
+            setTimeout(function () { computersTurn(); }, 1000);
        } 
        //Returning true is needed for our computersTurn() function to work.
        return true;
@@ -49,7 +49,7 @@ function placeXOrO(squareNumber) {
     function computersTurn() {
         //This boolean is needed for our while loop.
         let success = false;
-        //This variable staores a random number 0-8
+        //This variable stores a random number 0-8
         let pickASquare;
         //This condition allows our while loop to keep trying if a square is selected already.
         while (!success) {
@@ -67,45 +67,45 @@ function placeXOrO(squareNumber) {
     }
 
     //This function parses the selectedSquares array to search for win conditions.
-    //drawLine() function is called to draw a line on the screen if the condition is met.
-    function checkWinCondition() {
+    //drawWinLine() function is called to draw a line on the screen if the condition is met.
+    function checkWinConditions() {
         // X 0, 1, 2 condition.
-        if (arrayIncludes('0X', '1X', '2X')) { drawLine(50, 100, 558, 100) }
+        if (arrayIncludes('0X', '1X', '2X')) { drawWinLine(50, 100, 558, 100) }
         // X 3, 4, 5 condition.
-        else if (arrayIncludes('3X', '4X', '5X')) { drawLine(50, 304, 558, 304) }
+        else if (arrayIncludes('3X', '4X', '5X')) { drawWinLine(50, 304, 558, 304) }
         // X 6, 7, 8 condition.
-        else if (arrayIncludes('6X', '7X', '8X')) { drawLine(50, 508, 558, 508) }
+        else if (arrayIncludes('6X', '7X', '8X')) { drawWinLine(50, 508, 558, 508) }
         // X 0, 3, 6 condition.
-        else if (arrayIncludes('0X', '3X', '6X')) { drawLine(100, 50, 100, 558) }
+        else if (arrayIncludes('0X', '3X', '6X')) { drawWinLine(100, 50, 100, 558) }
         // X 1, 4, 7 condition.
-        else if (arrayIncludes('1X', '4X', '7X')) { drawLine(304, 50, 304, 558) }
+        else if (arrayIncludes('1X', '4X', '7X')) { drawWinLine(304, 50, 304, 558) }
         // X 2, 5, 8 condition.
-        else if (arrayIncludes('2X', '5X', '8X')) { drawLine(508, 50, 508, 558) }
+        else if (arrayIncludes('2X', '5X', '8X')) { drawWinLine(508, 50, 508, 558) }
         // X 6, 4, 2 condition.
-        else if (arrayIncludes('6X', '4X', '2X')) { drawLine(100, 508, 510, 90) }
+        else if (arrayIncludes('6X', '4X', '2X')) { drawWinLine(100, 508, 510, 90) }
         // X 0, 4, 8 condition.
-        else if (arrayIncludes('0X', '4X', '8X')) { drawLine(100, 100, 520, 520) }
+        else if (arrayIncludes('0X', '4X', '8X')) { drawWinLine(100, 100, 520, 520) }
         // O 0, 1, 2 condition.
-        else if (arrayIncludes('0O', '1O', '2O')) { drawLine(50, 100, 558, 100) }
+        else if (arrayIncludes('0O', '1O', '2O')) { drawWinLine(50, 100, 558, 100) }
         // O 3, 4, 5 condition.
-        else if (arrayIncludes('3O', '4O', '5O')) { drawLine(50, 304, 558, 304) }
+        else if (arrayIncludes('3O', '4O', '5O')) { drawWinLine(50, 304, 558, 304) }
         // O 6, 7, 8 condition.
-        else if (arrayIncludes('6O', '7O', '8O')) { drawLine(50, 508, 558, 508) }
+        else if (arrayIncludes('6O', '7O', '8O')) { drawWinLine(50, 508, 558, 508) }
         // O 0, 3, 6 condition.
-        else if (arrayIncludes('0O', '3O', '6O')) { drawLine(100, 50, 100, 558) }
+        else if (arrayIncludes('0O', '3O', '6O')) { drawWinLine(100, 50, 100, 558) }
         // O 1, 4, 7 condition.
-        else if (arrayIncludes('1O', '4O', '7O')) { drawLine(304, 50, 304, 558) }
+        else if (arrayIncludes('1O', '4O', '7O')) { drawWinLine(304, 50, 304, 558) }
         // O 2, 5, 8 condition.
-        else if (arrayIncludes('2O', '5O', '8O')) { drawLine(508, 50, 508, 558) }
+        else if (arrayIncludes('2O', '5O', '8O')) { drawWinLine(508, 50, 508, 558) }
         // O 6, 4, 2 condition.
-        else if (arrayIncludes('6O', '4O', '2O')) { drawLine(100, 508, 510, 90) }
+        else if (arrayIncludes('6O', '4O', '2O')) { drawWinLine(100, 508, 510, 90) }
         // O 0, 4, 8 condition.
-        else if (arrayIncludes('0O', '4O', '8O')) { drawLine(100, 100, 520, 520) }
+        else if (arrayIncludes('0O', '4O', '8O')) { drawWinLine(100, 100, 520, 520) }
         //This condition checks for a tie. If none of the above conditions are met and
         //9 squares are selected the code executes.
-        else if (selectedSquares.length >= 9) {
+        else if (selectedSquares.length >= 10) {
             //This function plays the tie game sound.
-            Audio('./media/place.mp3');
+            audio('./media/place.mp3');
             //This function sets a .3 second timer before the resetGame is called.
             setTimeout(function () {resetGame(); }, 500);
         }
@@ -117,7 +117,7 @@ function placeXOrO(squareNumber) {
             const b = selectedSquares.includes(squareB);
             const c = selectedSquares.includes(squareC);
             //If the 3 variables we pass are all included in our array then
-            //true is returned and our else if condition executes the drawLine() function.
+            //true is returned and our else if condition executes the drawWinLine() function.
             if (a === true && b === true && c === true) { return true; }
         }
     }
@@ -131,7 +131,7 @@ function placeXOrO(squareNumber) {
     }
 
     //This function takes a string parameter of the path you set earlier for
-    //placement sound('.media/place.mp3')
+    //placement sound('.med ia/place.mp3')
     function audio(audioURL) {
         //We create a new audio object and we pass the path as a patemeter.
         let audio = new Audio(audioURL);
@@ -140,7 +140,7 @@ function placeXOrO(squareNumber) {
     }
 
     //This function utilizes HTML canvas to draw winn lines.
-    function drawLine(coordX1, coordY1, coordX2, coordY2) {
+    function drawWinLine(coordX1, coordY1, coordX2, coordY2) {
         //This line accesses our HTML canvas element.
         const canvas = document.getElementById('win-lines');
         //This line gives us access to methods and properties to use on canvas.
@@ -207,12 +207,12 @@ function placeXOrO(squareNumber) {
     //This line plays the win sound.
     audio('./media/mellow.mp3');
     //This line calls our main animation loop.
-    animateLineDrawing();
+    animationLineDrawing();
     //This line waits 1 second. Then, clears canvas, resets game, an allows clicking again.
     setTimeout(function () { clear(); resetGame(); }, 1000);
 }
 
-//This function resets the game in the event of a tie or win.
+//This function s the game in the event of a tie or win.
 function resetGame() {
     //This for loop iterates through each HTML square element.
     for (let i = 0; i < 9; i++) {
