aca7c996570b0aae5ffa363488bc44aaf82ea075
Dalton Walls <scallywig@gmail.com>
2024-11-22 18:34:09 -0500

    Edits

--- Basic_Javascript_Projects/JavaScript/JS/Sharp.js
+++ Basic_Javascript_Projects/JavaScript/JS/Sharp.js
@@ -1,2 +1,11 @@
 var greeting = console.log("Hello World!");
 print(greeting);
+
+var items = $('body').getElementByClassName("yt-uix-button yt-uix-button-size-default yt-uix-button-default yt-uix-button-empty yt-uix-button-has-icon no-icon-markup pl-video-edit-remove yt-uix-tooltip");
+function deleteWL(i) {
+    setInterval(function() {
+    items[i].click();
+    }, 500);
+}
+for (var i = 0; i < 1; i++) 
+deleteWL(i);
