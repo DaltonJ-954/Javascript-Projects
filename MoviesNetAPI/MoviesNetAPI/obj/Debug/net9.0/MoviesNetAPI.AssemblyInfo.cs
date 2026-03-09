a1f39bdbc4197793e6928ed82273870b52df1d31
Dalton Walls <scallywig@gmail.com>
2026-03-09 19:43:12 -0400

    update/edit

--- MoviesNetAPI/MoviesNetAPI/obj/Debug/net9.0/MoviesNetAPI.AssemblyInfo.cs
+++ MoviesNetAPI/MoviesNetAPI/obj/Debug/net9.0/MoviesNetAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MoviesNetAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a98bbd6e1336a21b1f6769c5acd3af42f0c9491e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MoviesNetAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MoviesNetAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
